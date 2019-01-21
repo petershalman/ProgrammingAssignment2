b5968c7856034c4815b7d31c5dcb8d93d609689e
Payam <peter.shalman@gmail.com>
2019-01-21 15:33:53 +0000

    Coursera Data Analysis Course, Week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrixi <- function(x = matrix()) {
+        inv_mat <- NULL
+        set_mat <- function(mat) {
+                x <<- mat
+                inv_mat <<- NULL
+        }
+        get_mat <- function() x
+        setInverse_mat <- function(inverse_mat) inv_mat <<- inverse_mat
+        getInverse_mat <- function() inv_mat
+        list(set = set_mat,
+             get = get_mat,
+             setInverse = setInverse_mat,
+             getInverse = getInverse_mat)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolvei <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv_mat <- x$getInverse()
+        if (!is.null(inv_mat)) {
+                message("getting cached data")
+                return(inv_mat)
+        }
+        fmat <- x$get()
+        inv_mat <- solve(fmat, ...)
+        x$setInverse(inv_mat)
+        inv_mat
 }
